3f50b8310b847a919d72f03e9f33e876bd1a20d2
golbkim <dhdiagram@gmail.com>
2020-01-13 13:08:24 +0900

    code sync

---
+++ shop/models.py
@@ -0,0 +1,46 @@
+from django.db import models
+from django.urls import reverse
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=200, db_index=True)
+    slug = models.SlugField(max_length=200, db_index=True, unique=True, allow_unicode=True)
+    meta_description = models.CharField(max_length=250, blank=True)
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('shop:product_in_category',args=[self.slug])
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, related_name='category_products')
+    name = models.CharField(max_length=200, db_index=True)
+    slug = models.SlugField(max_length=200, db_index=True, unique=True, allow_unicode=True)
+    meta_description = models.TextField(max_length=250, blank=True)  # 검색엔진이 읽어가는 글자 수 제한
+
+    image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
+
+    price = models.IntegerField()
+    stock = models.IntegerField()
+    available_display = models.BooleanField(default=True)
+    available_order = models.BooleanField(default=True)
+
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created']
+        index_together = [['id','slug']]
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('shop:product_detail', args=[self.id, self.slug])
